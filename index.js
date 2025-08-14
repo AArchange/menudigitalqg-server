76a484ded8e956fd26d552b503b6df02459179df
Ange SITONDJI <angesitondji@gmail.com>
2025-08-14 15:27:52 +0100

    abonnement

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
 // server/index.js
 
-// 1. CHARGER LES VARIABLES D'ENVIRONNEMENT
 const dotenv = require('dotenv');
 dotenv.config();
 
-// 2. IMPORTER LES MODULES
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+
+// On importe nos fichiers de routes bien séparés
 const dishRoutes = require('./routes/dishRoutes');
 const authRoutes = require('./routes/authRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
 
-// 3. CONNECTER LA BASE DE DONNÉES
 connectDB();
 
-// 4. CRÉER L'APPLICATION EXPRESS
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// 5. APPLIQUER LES MIDDLEWARES
 app.use(cors());
 app.use(express.json());
 
-// 6. DÉFINIR LES ROUTES
 app.get('/', (req, res) => {
   res.send('API du Menu Digital QG fonctionne !');
 });
 
+// On dit à Express d'utiliser chaque routeur pour son chemin spécifique
 app.use('/api/dishes', dishRoutes);
-app.use('/api/auth', authRoutes); // Contient register, login, et profile
-app.use('/api/payments', paymentRoutes); // Contient la vérification de paiement
+app.use('/api/auth', authRoutes);
+app.use('/api/payments', paymentRoutes);
 
-// 7. DÉMARRER LE SERVEUR
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });
